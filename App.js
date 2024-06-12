2a4411e0e1ba7fc65e166e3f9d33535cfe2de7cc
yunusf <yunusf42@gmail.com>
2024-06-12 21:31:04 +0700

    added screens editNote and addNote

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react'
 import Home from './src/screens/home'
+import AddNote from './src/screens/addNote'
+import EditNote from './src/screens/editNote'
+
+const CurrentPageWidget = ({
+  currentPage, noteList, setCurrentPage
+}) => {
+  switch (currentPage) {
+    case 'home':
+      return (
+        <Home
+          noteList={noteList}
+          setCurrentPage={setCurrentPage}
+        />
+      )
+    case 'add':
+      return <AddNote />
+    case 'edit':
+      return <EditNote />
+    default:
+      return <Home />
+  }
+}
 
 const App = () => {
+  const [currentPage, setCurrentPage] = useState('home')
+
   const [noteList, setNoteList] = useState([
     {
       id: 1,
@@ -10,7 +34,13 @@ const App = () => {
     },
   ])
 
-  return <Home noteList={noteList}/>
+  return (
+    <CurrentPageWidget
+      currentPage={currentPage}
+      setCurrentPage={setCurrentPage}
+      noteList={noteList}
+    />
+  )
 }
 
 export default App
---
+++ src/screens/addNote.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { Text } from 'react-native'
+
+const AddNote = () => <Text>Tambahkan Note</Text>
+
+export default AddNote
